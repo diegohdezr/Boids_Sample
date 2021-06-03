f1559baf1e5bcdd73f55de135ac818bbad544703
diegohdezr <diego.hdez@hotmail.com>
2021-06-03 18:53:21 -0500

    spawner code modified
    first mod to spawner code, just spawns 100 boids withouth any other behavior

--- BOIDS/Assets/Scripts/Spawner.cs
+++ BOIDS/Assets/Scripts/Spawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,15 +9,45 @@ using UnityEngine;
  */
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static public Spawner SpawnerSingleton;
+    static public List<Boid> boids;
+
+    [Header("Set in Inspector: Spawning")]
+    public GameObject       boidPrefab;
+    public Transform        boidAnchor;
+    public int              numBoids = 100;
+    public float            spawnRadius = 100f;
+    public float            spawnDelay = 0.1f;
+
+    [Header("Set in inspector: Boid Behaviour")]
+    public float            Velocity = 30f;
+    public float            neighbourDistance = 30f;
+    public float            CollissionDist = 4f;
+    public float            velMatching = 0.25f;
+    public float            flockCentering = 0.2f;
+    public float            collAvoid = 2f;
+    public float            attractPull = 2f;
+    public float            attractPush = 2f;
+    public float            attractPushDistance = 5f;
+
+    private void Awake()
     {
-        
+        //set this GO as the singleton
+        SpawnerSingleton = this;
+        //start the instantiation of the BOIDS
+        boids = new List<Boid>();
+        InstantiateBoid();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InstantiateBoid()
     {
-        
+        GameObject go = Instantiate(boidPrefab);
+        Boid b = go.GetComponent<Boid>();
+        b.transform.SetParent(boidAnchor);
+        boids.Add(b);
+        if (boids.Count < numBoids)
+        {
+            Invoke("InstantiateBoid", spawnDelay);
+        }
     }
 }

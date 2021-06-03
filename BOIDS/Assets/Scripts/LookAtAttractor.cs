4e54c3ccfa61894d1a9770ba7584c7bc6fd9a1a1
diegohdezr <diego.hdez@hotmail.com>
2021-06-03 18:32:49 -0500

    Attractor script and look at attractor script

--- BOIDS/Assets/Scripts/LookAtAttractor.cs
+++ BOIDS/Assets/Scripts/LookAtAttractor.cs
@@ -9,15 +9,10 @@ using UnityEngine;
 
 public class LookAtAttractor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(Attractor.POS);
     }
 }

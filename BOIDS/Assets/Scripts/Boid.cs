1ad30ad361bb6ad926d0f5d0363d864a449711ef
diegohdezr <diego.hdez@hotmail.com>
2021-06-03 19:14:14 -0500

    first update to Boids objects
    spawn the boids
    set their spawn position
    set boid and boid trail color

--- BOIDS/Assets/Scripts/Boid.cs
+++ BOIDS/Assets/Scripts/Boid.cs
@@ -8,15 +8,51 @@ using UnityEngine;
 //understanding of the world
 public class Boid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody            rigid;
+
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+
+        //set random initial position based on spawn radius
+        pos = Random.insideUnitSphere * Spawner.SpawnerSingleton.spawnRadius;
+
+        //set a random initial velocity
+        Vector3 vel = Random.onUnitSphere * Spawner.SpawnerSingleton.Velocity;
+        rigid.velocity = vel;
+
+        LookAhead();
+
+        //give the void a random color
+        /* r = 0 - 1  -> 0.1231
+         * g = 0 - 1  -> 0.2343
+         * b = 0 - 1  -> 0.7898
+         * r+g+b
+         */
+        Color randCol = Color.black;
+        while (randCol.r+randCol.g+randCol.b<1f) 
+        {
+            randCol = new Color(Random.value, Random.value, Random.value);
+        }
+        Renderer[] rends = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer rend in rends)
+        {
+            rend.material.color = randCol;
+        }
+        TrailRenderer tRend = GetComponent<TrailRenderer>();
+        tRend.material.SetColor("_TintColor", randCol);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LookAhead()
     {
-        
+        //orient the Boid to look at the direction it is flying
+        transform.LookAt(pos+rigid.velocity);
     }
+
+    public Vector3 pos 
+    {
+        get { return transform.position; }
+        set { transform.position = value; }
+    }
+
 }

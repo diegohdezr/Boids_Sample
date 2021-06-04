d96615584b3510a2ff38e1252a9d5e2a77de6e5e
diegohdezr <diego.hdez@hotmail.com>
2021-06-03 19:54:35 -0500

    collision detection between boids

--- BOIDS/Assets/Scripts/Neighborhood.cs
+++ BOIDS/Assets/Scripts/Neighborhood.cs
@@ -9,15 +9,101 @@ using UnityEngine;
 */
 public class Neighborhood : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set dynamically in runtime")]
+    public List<Boid>           neighbors;
+    private SphereCollider      coll;
+
+    private void Start()
     {
+        neighbors = new List<Boid>();
+        coll = GetComponent<SphereCollider>();
+        coll.radius = Spawner.SpawnerSingleton.neighbourDistance / 2;
+    }
+    private void FixedUpdate()
+    {
+        if (coll.radius != Spawner.SpawnerSingleton.neighbourDistance / 2) 
+        {
+            coll.radius = Spawner.SpawnerSingleton.neighbourDistance / 2;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Boid b = other.GetComponent<Boid>();
+        if (b != null) 
+        {
+            if (neighbors.IndexOf(b) == -1) 
+            {
+                //if the boid that we collided with is not a registered neighbor, make it a neighbor
+                neighbors.Add(b);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Boid b = other.GetComponent<Boid>();
+        if (b != null)
+        {
+            if (neighbors.IndexOf(b) != -1)
+            {
+                //if the boid that we collided with is not a registered neighbor, make it a neighbor
+                neighbors.Remove(b);
+            }
+        }
+    }
+
+    public Vector3 avgPos
+    {
+        get 
+        {
+            Vector3 avg = Vector3.zero;
+            if (neighbors.Count == 0) { return avg; }
+            for (int i = 0; i < neighbors.Count; i++) 
+            {
+                avg += neighbors[i].pos;
+            }
+            avg /= neighbors.Count;
+            return avg;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 avgVel
     {
-        
+        get 
+        {
+            Vector3 avg = Vector3.zero;
+            if (neighbors.Count == 0) return avg;
+            for (int i = 0; i < neighbors.Count; i++) 
+            {
+                avg += neighbors[i].rigid.velocity;
+            }
+            avg /= neighbors.Count;
+            return avg;
+        }
     }
+
+    public Vector3 avgClosePos 
+    {
+        get 
+        {
+            
+            Vector3 avg = Vector3.zero;
+            Vector3 delta;
+            int nearCount = 0;
+            for (int i = 0; i < neighbors.Count; i++)
+            {
+                delta = neighbors[i].pos - transform.position;
+                if (delta.magnitude <= Spawner.SpawnerSingleton.CollissionDist) 
+                {
+                    avg += neighbors[i].pos;
+                    nearCount++;
+                }
+            }
+            if (nearCount == 0) return avg;
+            avg /= nearCount;
+            return avg;
+        }
+    }
+
 }
